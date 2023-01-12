aede84cc628acae4c970d31ad632c49ed52c32f1
amogusimpostor <99112532+theplantman@users.noreply.github.com>
2023-01-12 17:24:08 +0800

    Add files via upload

---
+++ Animation_Scraper.lua
@@ -0,0 +1,23 @@
+repeat
+    task.wait()
+until game:IsLoaded()
+local Scraped = {}
+if isfile(tostring(game.PlaceId) .. "_Scraped.json") then
+    Scraped = game.HttpService:JSONDecode(readfile(tostring(game.PlaceId) .. "_Scraped.json"))
+end
+function CheckIfScraped(AnimationId)
+    for Index, ScrapedAnimationId in pairs(Scraped) do
+        if AnimationId == ScrapedAnimationId then
+            return true
+        end
+    end
+end
+for Index, Animation in pairs(game:GetDescendants()) do
+    if Animation.ClassName == "Animation" and not CheckIfScraped(Animation.AnimationId) and tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)) and tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)) ~= 0 then
+        local AssetName = game.MarketplaceService:GetProductInfo(tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)))["Name"]
+        if AssetName then
+            Scraped[AssetName] = Animation.AnimationId
+        end
+    end
+end
+writefile(tostring(game.PlaceId) .. "_Scraped.json", game.HttpService:JSONEncode(Scraped))
