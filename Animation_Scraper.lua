2867cd59142de00d36d6d0b0970c8a41067ce3f1
amogusimpostor <99112532+theplantman@users.noreply.github.com>
2023-01-13 14:59:12 +0800

    Update Animation_Scraper.lua

--- Animation_Scraper.lua
+++ Animation_Scraper.lua
@@ -5,18 +5,32 @@ local Scraped = {}
 if isfile(game.PlaceId .. "_Scraped.json") then
     Scraped = game.HttpService:JSONDecode(readfile(game.PlaceId .. "_Scraped.json"))
 end
-function CheckIfScraped(AnimationId)
-    for Index, ScrapedAnimationId in pairs(Scraped) do
-        if AnimationId == ScrapedAnimationId then
-            return true
+function InsertAnimation(Animation)
+    if Animation then
+        local function CheckIfScraped(AnimationId)
+            for Index, ScrapedAnimationId in pairs(Scraped) do
+                if AnimationId == ScrapedAnimationId then
+                    return true
+                end
+            end
         end
-    end
+        if not CheckIfScraped(Animation.AnimationId) and tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)) and tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)) ~= 0 then
+            local AssetName = game.MarketplaceService:GetProductInfo(tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)))["Name"]
+            if AssetName then
+                Scraped[AssetName] = Animation.AnimationId
+            end
+        end
+    end 
 end
 for Index, Animation in pairs(game:GetDescendants()) do
-    if Animation.ClassName == "Animation" and not CheckIfScraped(Animation.AnimationId) and tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)) and tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)) ~= 0 then
-        local AssetName = game.MarketplaceService:GetProductInfo(tonumber(Animation.AnimationId:sub(14, #Animation.AnimationId)))["Name"]
-        if AssetName then
-            Scraped[AssetName] = Animation.AnimationId
+    if Animation.ClassName == "Animation" then
+        InsertAnimation(Animation)
+    end
+end
+for Index, Player in pairs(game.Players:GetPlayers()) do
+    if Player.Character and Player.Character:FindFirstChild("Humanoid") then
+        for Index, AnimationTrack in pairs(Player.Character.Humanoid:GetPlayingAnimationTracks()) do
+            InsertAnimation(AnimationTrack.Animation)
         end
     end
 end
